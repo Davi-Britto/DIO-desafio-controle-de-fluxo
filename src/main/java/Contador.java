e45b172b11f21eaec80a2c027e9e6c13f7e4ca0d
Davi-Britto <davi.britto@sptech.school>
2024-06-27 14:48:59 -0300

    Projeto finalizado

--- src/main/java/Contador.java
+++ src/main/java/Contador.java
@@ -14,7 +14,6 @@ public class Contador {
             contar(parametroUm, parametroDois);
 
         }catch (ParametrosInvalidosException exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
 
@@ -28,7 +27,7 @@ public class Contador {
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
         for (int i = 1; i <= contagem; i++){
-            System.out.println(i);
+            System.out.println("Imprimindo o número " + i);
         }
     }
 }
